2256a36ad1ec00708a9c85e1eeb36833567fc08c
Иван Былёв <ivan.bylyov@gmail.com>
2024-08-29 13:16:39 +0300

    Вся неинтерфейсная логика перенесена во Viewmodel

--- app/src/main/java/com/vortex/android/speedtestapp/ui/test/TestFragment.kt
+++ app/src/main/java/com/vortex/android/speedtestapp/ui/test/TestFragment.kt
@@ -1,12 +1,27 @@
 package com.vortex.android.speedtestapp.ui.test
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.google.android.material.snackbar.Snackbar
+import com.vortex.android.speedtestapp.R
 import com.vortex.android.speedtestapp.databinding.FragmentTestBinding
+import fr.bmartel.speedtest.SpeedTestReport
+import fr.bmartel.speedtest.SpeedTestSocket
+import fr.bmartel.speedtest.inter.ISpeedTestListener
+import fr.bmartel.speedtest.model.SpeedTestError
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
 
 //@AndroidEntryPoint
 class TestFragment : Fragment() {
@@ -17,7 +32,8 @@ class TestFragment : Fragment() {
             "Cannot access binding because it is null. Is the view visible?"
         }
     private val testViewModel: TestViewModel by viewModels()
-
+    private val downloadUrl = "http://speedtest.tele2.net/100MB.zip"
+    private val uploadUrl = "http://speedtest.karwos.net:8080/speedtest/upload.php"
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,13 +43,93 @@ class TestFragment : Fragment() {
 
         _binding = FragmentTestBinding.inflate(inflater, container, false)
 
-
-
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        listenToChannels()
+        UpdateUi()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun UpdateUi() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                testViewModel.instUploadSpeed.collectLatest {
+                    binding.instUploadSpeed.text = it
+                }
+            }
+        }
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                testViewModel.instDownloadSpeed.collectLatest {
+                    binding.instDownloadSpeed.text = it
+                }
+            }
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                testViewModel.averageDownloadSpeed.collectLatest {
+                    binding.averageDownloadSpeed.text = it
+                }
+            }
+        }
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                testViewModel.averageUploadSpeed.collectLatest {
+                    binding.averageUploadSpeed.text = it
+                }
+            }
+        }
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                testViewModel.isTestOngoing.collectLatest { isTestOngoing ->
+                    binding.btnStartTest.apply {
+                        isEnabled = !isTestOngoing
+                        isClickable = !isTestOngoing
+                    }
+                }
+            }
+        }
+
+        binding.apply {
+            if (testViewModel.buttonWasPushed) {
+                visibilityGroup.isVisible = true
+            }
+            downloadUrlTextview.text = "Download URL: $downloadUrl"
+            uploadUrlTextview.text = "Upload URL: $uploadUrl"
+            btnStartTest.setOnClickListener {
+                testViewModel.buttonWasPushed = true
+                visibilityGroup.isVisible = true
+                testViewModel.run {
+                    instUploadSpeed.value = ""
+                    instDownloadSpeed.value = ""
+                    averageUploadSpeed.value = ""
+                    averageDownloadSpeed.value = ""
+                }
+                testViewModel.startDownloadTest()
+            }
+        }
+    }
+
+    private fun listenToChannels() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            testViewModel.allEventsFlow.collect { event ->
+                when(event){
+                    is TestViewModel.AllEvents.Error -> {
+                        Snackbar
+                            .make(binding.root, getString(R.string.error_empty_password), Snackbar.LENGTH_SHORT)
+                            .setAnchorView(binding.btnStartTest)
+                            .show()
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/vortex/android/speedtestapp/ui/test/TestViewModel.kt
+++ app/src/main/java/com/vortex/android/speedtestapp/ui/test/TestViewModel.kt
@@ -1,17 +1,111 @@
 package com.vortex.android.speedtestapp.ui.test
 
+import android.util.Log
+import androidx.annotation.StringRes
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.viewModelScope
+import com.google.android.material.snackbar.Snackbar
+import com.vortex.android.speedtestapp.R
 import dagger.hilt.android.lifecycle.HiltViewModel
+import fr.bmartel.speedtest.SpeedTestReport
+import fr.bmartel.speedtest.SpeedTestSocket
+import fr.bmartel.speedtest.inter.ISpeedTestListener
+import fr.bmartel.speedtest.model.SpeedTestError
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 
 //@HiltViewModel
 class TestViewModel : ViewModel() {
 
+    private val downloadUrl = "http://speedtest.tele2.net/100MB.zip"
+    private val uploadUrl = "http://speedtest.karwos.net:8080/speedtest/upload.php"
+
+    private val eventsChannel = Channel<AllEvents>()
+    val allEventsFlow = eventsChannel.receiveAsFlow()
+
     private val _text = MutableStateFlow<String>("This is Test Fragment")
     val text : StateFlow<String>
         get() = _text.asStateFlow()
+
+    var buttonWasPushed : Boolean = false
+
+    val instDownloadSpeed = MutableStateFlow<String>("")
+
+    val averageDownloadSpeed = MutableStateFlow<String>("")
+
+    val instUploadSpeed = MutableStateFlow<String>("")
+
+    val averageUploadSpeed = MutableStateFlow<String>("")
+
+    val isTestOngoing = MutableStateFlow<Boolean>(false)
+
+
+    fun startDownloadTest() {
+        isTestOngoing.value = true
+        viewModelScope.launch(Dispatchers.IO) {
+
+            val speedTestSocket = SpeedTestSocket()
+
+            speedTestSocket.addSpeedTestListener(object : ISpeedTestListener {
+                override fun onCompletion(report: SpeedTestReport) {
+                    averageDownloadSpeed.value = String.format("%.2f", report.transferRateBit.toDouble()/(1024*1024))
+                    startUploadTest()
+                }
+
+                override fun onError(speedTestError: SpeedTestError, errorMessage: String) {
+                    viewModelScope.launch {
+                        eventsChannel.send(AllEvents.Error(0))
+                    }
+                    isTestOngoing.value = false
+                    Log.d("download_tag","CONNECTION ERROR: $errorMessage")
+                }
+
+                override fun onProgress(percent: Float, report: SpeedTestReport) {
+                    instDownloadSpeed.value = String.format("%.2f", report.transferRateBit.toDouble()/(1024*1024))
+                }
+            })
+            speedTestSocket.startDownload(downloadUrl, 200)
+        }
+    }
+
+    private fun startUploadTest() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val speedTestSocket = SpeedTestSocket()
+
+            speedTestSocket.addSpeedTestListener(object : ISpeedTestListener {
+                override fun onCompletion(report: SpeedTestReport) {
+                    averageUploadSpeed.value = String.format("%.2f", report.transferRateBit.toDouble()/(1024*1024))
+                    isTestOngoing.value = false
+                }
+
+                override fun onError(speedTestError: SpeedTestError, errorMessage: String) {
+                    viewModelScope.launch {
+                        eventsChannel.send(AllEvents.Error(0))
+                    }
+                    isTestOngoing.value = false
+                    Log.d("upload_tag","CONNECTION ERROR: $errorMessage, $speedTestError")
+                }
+
+                override fun onProgress(percent: Float, report: SpeedTestReport) {
+                    instUploadSpeed.value = String.format("%.2f", report.transferRateBit.toDouble()/(1024*1024))
+                }
+            })
+            speedTestSocket.startUpload(uploadUrl, 1000000, 200)
+        }
+    }
+
+    sealed class AllEvents {
+        //Viewmodel не должна работать со строками
+        //data class Message(@StringRes val messageRes : Int) : AllEvents()
+        //data class ErrorCode(val code : Int): AllEvents()
+        data class Error(@StringRes val errorRes : Int) : AllEvents()
+    }
 }
